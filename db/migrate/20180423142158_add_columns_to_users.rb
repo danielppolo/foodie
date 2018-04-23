71d8711842321f3f1fbf3e8038729a2e58a27e83
Daniel Polo <od.polo@gmail.com>
2018-04-23 16:38:29 +0200

    User model completed

--- db/migrate/20180423142158_add_columns_to_users.rb
+++ db/migrate/20180423142158_add_columns_to_users.rb
@@ -3,6 +3,6 @@ class AddColumnsToUsers < ActiveRecord::Migration[5.1]
     add_column :users, :name, :string
     add_column :users, :age, :integer
     add_column :users, :gender, :integer
-    add_column :users, :radius_search, :integer
+    add_column :users, :radius_search, :integer, default: 2
   end
 end
