98351de41077940c45d6d2b94bfcf36195ec0523
Daniel Polo <od.polo@gmail.com>
2018-04-27 18:06:43 +0200

    Without if in cate

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Restaurant.delete_all
 city = "milano"
 BASE = "https://www.foodora.it/en"
 city_url = "https://www.foodora.it/en/city/#{city}"
-n_restaurants = 8
+n_restaurants = 20
 i = 0
 
 Meal.delete_all
