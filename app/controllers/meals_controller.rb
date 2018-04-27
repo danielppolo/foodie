eab46b79caf0c4f3e0f8a65e263d2ca05560eaec
Daniel Polo <od.polo@gmail.com>
2018-04-27 17:14:07 +0200

    Stripe btn styling

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -2,7 +2,6 @@ class MealsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :filter]
 
   def index
-    @categories = Meal.categories(10)
     @lat = cookies[:lat]
     @lng = cookies[:lng]
     @meals = Meal.filter(params, cookies).first(3)
