6c771fea89e4d15f7dedf23a9db8a9bd7608285f
Daniel Polo <od.polo@gmail.com>
2018-04-29 14:21:47 +0200

    Seeds Meal w/category

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -4,7 +4,8 @@ class MealsController < ApplicationController
   def filter
     @cookies = cookies
     @categories = Meal.categories(12)
-    @nearby_categories = Meal.nearby_categories(12, cookies)
+    radius_search = user_signed_in? ? current_user.radius_search : 10
+    @nearby_categories = Meal.nearby_categories(12, cookies, radius_search)
     @lat = cookies[:lat]
     @lng = cookies[:lng]
   end
@@ -12,8 +13,9 @@ class MealsController < ApplicationController
   def index
     @lat = cookies[:lat]
     @lng = cookies[:lng]
-    # @meals = Meal.filter(params, cookies).first(10)
-    @meals = Meal.all.shuffle.first(10)
+    radius_search = user_signed_in? ? current_user.radius_search : 10
+    @meals = Meal.filter(params, cookies, radius_search).first(10)
+    # @meals = Meal.all.shuffle.first(10)
   end
 
 
