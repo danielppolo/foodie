fdecabf9d2e86a10a56105acc52e434c224bba26
Daniel Polo <od.polo@gmail.com>
2018-04-30 12:18:03 +0200

    Minor

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -3,19 +3,26 @@ class MealsController < ApplicationController
 
   def search
     @cookies = cookies
-    @categories = Meal.categories(12)
-    radius_search = user_signed_in? ? current_user.radius_search : 10
-    @nearby_categories = Meal.nearby_categories(12, cookies, radius_search)
     @lat = cookies[:lat]
     @lng = cookies[:lng]
+    radius_search = user_signed_in? ? current_user.radius_search : 5
+    @nearby_categories = Meal.nearby_categories(12, cookies, radius_search)
+    if @lat && @lng && @nearby_categories != []
+      @categories = @nearby_categories
+    else
+      @categories = Meal.categories(12)
+    end
   end
 
   def index
     @lat = cookies[:lat]
     @lng = cookies[:lng]
-    radius_search = user_signed_in? ? current_user.radius_search : 10
-    @meals = Meal.filter(params, cookies, radius_search).first(20)
-    # @meals = Meal.all.shuffle.first(10)
+    radius_search = user_signed_in? ? current_user.radius_search : 5
+    if cookies[:lat] && cookies[:lng]
+      @meals = Meal.filter(params, cookies, radius_search).first(20)
+    else
+      @meals = Meal.all.shuffle.first(10)
+    end
   end
 
 
