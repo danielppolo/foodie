b4cd170334d1b5725e73f3b8b3207ed962013c77
Juan Paredes <juanchuu.jfpc@gmail.com>
2018-04-30 11:38:56 +0200

    patch facebook connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,7 @@ class User < ApplicationRecord
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
     user_params[:facebook_picture_url] = auth.info.image
-    user_par def index
-  endams[:token] = auth.credentials.token
+    user_params[:token] = auth.credentials.token
     user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
     user_params = user_params.to_h
 
