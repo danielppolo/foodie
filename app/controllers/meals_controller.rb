624b9535aa5e8e96e51a44b9a8fa963ae4dbee04
Daniel Polo <od.polo@gmail.com>
2018-05-01 13:20:09 +0200

    Corrections

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -21,10 +21,10 @@ class MealsController < ApplicationController
   def index
     @lat = cookies[:lat]
     @lng = cookies[:lng]
-    radius_search = user_signed_in? ? current_user.radius_search : 5
+    search_radius = user_signed_in? ? current_user.radius_search : 5
     @paths = {}
     if cookies[:lat] && cookies[:lng]
-      @meals = Restaurant.find(357).meals.first(1)
+      @meals = Meal.filter(params, cookies, search_radius).first(10)
       @meals.each do |m|
         url = 'https://maps.googleapis.com/maps/api/directions/json?origin=' + @lat.to_s + ',' + @lng.to_s + '&destination=' + m.restaurant.latitude.to_s + ',' + m.restaurant.longitude.to_s + '&key=AIzaSyBsCPWcOcjt6XbMm6MOsRretGjkgclnWZk'
         route_serialized = open(url).read
@@ -34,11 +34,9 @@ class MealsController < ApplicationController
           link = URI.escape(points, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
           @paths[m.id] = link
         end
-      else
-        return redirect_to root_path
       end
+    else
+        return redirect_to root_path
     end
   end
-
-
 end
