36b27b1fecdbffbee25fec657e4f5058e704eae4
Daniel Polo <od.polo@gmail.com>
2018-05-03 09:18:12 +0200

    Conflicts w old new branches

--- app/javascript/packs/notify-order-deleted.js
+++
@@ -1,5 +0,0 @@
-import swal from 'sweetalert';
-document.addEventListener('DOMContentLoaded', function () {
- swal( "Order canceled!" ,  "The order has been canceled" ,  "success" );
- console.log("Success");
-});
